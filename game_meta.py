db04f5850faa6206fcc3fe4dad5ed8af8b85531d
Deorder <31899960+deorder@users.noreply.github.com>
2022-04-01 23:26:20 +0200

    Initial commit

---
+++ game_meta.py
@@ -0,0 +1,13 @@
+from ..basic_game import BasicGame
+
+
+class MetaGame(BasicGame):
+
+    Name = "Meta"
+    Author = "Deorder"
+    Version = "0.0.1"
+
+    GameName = "Meta"
+    GameShortName = "meta"
+    GameBinary = r"meta.json"
+    GameDataPath = r""
